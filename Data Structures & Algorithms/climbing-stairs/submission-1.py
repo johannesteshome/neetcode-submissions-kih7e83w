db1e26c5c44775a03874e3422ee7c3f78e9aae96
Yohannes Teshome Motuama <83913756+johannesteshome@users.noreply.github.com>
2026-05-19 14:44:40 +0200

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        memo = {}
+
+        def dp(i):
+            if i == 1:
+                return 1
+            
+            if i == 2:
+                return 2
+            
+            if i in memo:
+                return memo[i]
+            
+            memo[i] = dp(i-1) + dp(i-2)
+            return memo[i]
+        
+        return dp(n)
